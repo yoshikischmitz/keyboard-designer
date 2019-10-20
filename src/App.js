032a1907c0e768640210e68848304744362b3f53
Yoshiki Schmitz <yoshikischmitz@outlook.com>
2019-10-20 19:10:51 +0900

    add input and labels and niceities

--- src/App.js
+++ src/App.js
@@ -1,41 +1,89 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Box, Text } from "rebass";
+import { Input } from "@rebass/forms";
 
-const Icon = ({ char, focused }) => (
+const Icon = ({ char, focused, label }) => (
   <Box
-    width={24}
-    height={24}
+    width={36}
+    height={36}
     display="flex"
     alignItems="center"
     justifyContent="center"
+    flexDirection="column"
     sx={{
-      border: "1px solid black"
+      border: "1px solid black",
+      marginLeft: "-1px"
     }}
+    bg={focused && "black"}
+    color={focused && "white"}
   >
-    <Text fontWeight={focused && "bold"}>{char}</Text>
+    <Text fontWeight="500" mt="1px">
+      {char}
+    </Text>
+    <Text fontSize="8px" mt={-1}>
+      {label}
+    </Text>
   </Box>
 );
 
-const menuChars = ["w", "s", "f", "r"];
+const menuChars = ["w", "s", "f", "i", "e", "a"];
+const menuIcons = { a: "aA" };
+const menuLabels = {
+  w: "wrap",
+  s: "style",
+  f: "find",
+  i: "insert",
+  e: "edit",
+  a: "add",
+  "/": "cmd"
+};
+
+const Menu = ({ children }) => (
+  <Box mb="-1px" width={36 * menuChars.length}>
+    {children}
+  </Box>
+);
+
+const node = (component, props, ...children) => ({
+  component,
+  props,
+  children: children || []
+});
+
 const App = () => {
-  const [menuFocus, setMenuFocus] = useState();
+  const commandRef = useRef();
+  const [command, setCommand] = useState();
+  const [args, setArgs] = useState("");
+  commandRef.current = command;
+  const inputRef = useRef(null);
+
+  const [tree, setTree] = useState(
+    node("Box", { p: 3, bg: "red" }, node("Text", null, "I am a div"))
+  );
+
   useEffect(() => {
     document.addEventListener("keydown", e => {
       const { key } = e;
-      if (menuChars.includes(key)) {
-        setMenuFocus(key);
+      if (!commandRef.current && menuChars.includes(key)) {
+        e.preventDefault();
+        setCommand(key);
       }
-      if (key === "Escape") {
-        setMenuFocus(null);
+      if (commandRef.current && key === "Escape") {
+        setArgs("");
+        setCommand(null);
       }
     });
   }, []);
 
+  useEffect(() => {
+    inputRef.current && inputRef.current.focus();
+  }, [command]);
+
   return (
     <Box
       display="flex"
-      flexDirection="row"
-      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
       sx={{
         bottom: 3,
         left: 0,
@@ -43,9 +91,30 @@ const App = () => {
         position: "absolute"
       }}
     >
-      {menuChars.map(char => (
-        <Icon char={char} focused={menuFocus === char} />
-      ))}
+      {command && (
+        <Menu>
+          {console.log("input got rendered")}
+          <Input
+            ref={inputRef}
+            value={args}
+            onChange={e => setArgs(e.target.value)}
+            onKeyDown={e => {
+              if (e.key === "Enter") {
+                setArgs("");
+              }
+            }}
+          />
+        </Menu>
+      )}
+      <Box display="flex" flexDirection="row" justifyContent="center">
+        {menuChars.map(char => (
+          <Icon
+            char={menuIcons[char] || char}
+            focused={command === char}
+            label={menuLabels[char]}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
